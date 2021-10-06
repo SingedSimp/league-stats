1abaa216f2ea649b4ad0d84e6eeaa7fc1290e2a3
SingedSimp <firetamermail@gmail.com>
2021-10-06 15:49:42 -0700

    add start of electron GUI

---
+++ index.js
@@ -0,0 +1,40 @@
+const { app, BrowserWindow } = require('electron');
+
+/**
+ * Creates the main window
+ */
+function createMainWindow () {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: false,
+    }
+  });
+
+  win.loadFile('start.html');
+  win.webContents.openDevTools()
+}
+
+app.whenReady().then(createMainWindow);
+
+// When all windows are closed exit the app, except in macOS.
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+});
+
+// When the application gets activated create the main window if one does not exist
+app.on('activate', () => {
+  if (BrowserWindow.getAllWindows().length === 0) {
+    webPreferences.nodeIntegration = true;
+    createMainWindow({
+        webPreferences: {
+            nodeIntegration: true,
+            contextIsolation: false,
+        }
+    })
+  }
+});
