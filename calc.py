3b03cf871cd99f867cdfa538beae5ad08fe6eb41
SingedSimp <firetamermail@gmail.com>
2021-10-07 15:50:17 -0700

    Set base stats to calculate (ad, ap, ms, hp, tenacity, armor, mr)

--- calc.py
+++ calc.py
@@ -1,18 +1,67 @@
 import json
 import os
 print("Calculating...")
-toplethal = 0
-toptime = 0
+topms = -1
+topad = -1
+topap = -1
+tophp = -1
+toptenacity = -1
+toptime = -1
+topmr = -1
+toparmor = -1
 name = "none"
 for filename in os.listdir("./output"):
     if filename.endswith(".json"):
         data = json.load(open(f"./output/{filename}", "r"))
-        lethal = data['activePlayer']['championStats']['physicalLethality']
+        ms = data['activePlayer']['championStats']['moveSpeed']
+        ad = data['activePlayer']['championStats']['attackDamage']
+        ap = data['activePlayer']['championStats']['abilityPower']
+        hp = data['activePlayer']['championStats']['maxHealth']
+        tenacity = data['activePlayer']['championStats']['tenacity']
+        armor = data['activePlayer']['championStats']['armor']
+        mr = data['activePlayer']['championStats']['magicResist']
         time = data['gameData']['gameTime']
-        if lethal > toplethal:
-            toplethal = lethal
-            toptime = time
-            name = filename
-print(toplethal)
-print(f"{toptime / 60}")
-print(name)
+        if ms > topms:
+            topms = ms
+            mstime = time
+            msname = filename
+        if ad > topad:
+            topad = ad
+            adtime = time
+            adname = filename
+        if ap > topap:
+            topap = ap
+            aptime = time
+            apname = filename
+        if hp > tophp:
+            tophp = hp
+            hptime = time
+            hpname = filename
+        if tenacity > toptenacity:
+            toptenacity = tenacity
+            tenacitytime = time
+            tenacityname = filename
+        if mr > topmr:
+            topmr = mr
+            mrtime = time
+            mrname = filename
+        if armor > toparmor:
+            toparmor = armor
+            armortime = time
+            armorname = filename
+        
+ad = [topad, adtime, adname]
+ap = [topap, aptime, apname]
+ms = [topms, mstime, msname]
+hp = [tophp, hptime, hpname]
+tenacity = [toptenacity, tenacitytime, tenacityname]
+armor = [toparmor, armortime, armorname]
+mr = [topmr, mrtime, mrname]
+print(ad)
+print(ap)
+print(ms)
+print(hp)
+print(tenacity)
+print(armor)
+print(mr)
+
